cfdd40d200570b0e554506ee8e8665193520960b
David Benoit <david.j.benoit1@gmail.com>
2022-01-30 19:08:13 -0600

    add npm express

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let skill = document.querySelectorAll('.skill')
+const express = require('express')
+const app = express()
+const port = 3000
 
-skill.addEventListener('mouseover')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
