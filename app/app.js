c21f9f923ffc489f1b11760319c8970d71202a6a
David Benoit <david.j.benoit1@gmail.com>
2022-02-15 23:05:15 -0600

    Begin JS

---
+++ app/app.js
@@ -0,0 +1,16 @@
+const myName = document.querySelector('.name')
+let textInMyName = myName.textContent
+let myNameDes = document.querySelector('.name-des')
+
+console.log(myName)
+
+textInMyName.forEach(letter => {
+  
+});
+
+//create typewriter effect
+const typewriter = (content) => {
+
+}
+
+window.onload = typewriter(textInMyName)
